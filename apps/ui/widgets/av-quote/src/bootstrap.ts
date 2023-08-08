d6c78fa84a34564b2c4cd5b38361ff449b79d3b6
Manfred Steyer <manfred.steyer@gmx.net>
2023-08-08 10:08:30 +0200

    switch to api for bootstrapping wc

--- apps/ui/widgets/av-quote/src/bootstrap.ts
+++ apps/ui/widgets/av-quote/src/bootstrap.ts
@@ -1,17 +1,25 @@
-import { bootstrapApplication } from "@angular/platform-browser";
+import { createApplication } from "@angular/platform-browser";
 import { appConfig } from "./app/app.config";
 import { AppComponent } from "./app/app.component";
-import { ApplicationRef } from "@angular/core";
 import { createCustomElement } from "@angular/elements";
 import { connectRouter } from "@angular-architects/module-federation-tools";
 import { Router } from "@angular/router";
 
-bootstrapApplication(AppComponent, appConfig)
-  .then((appRef: ApplicationRef) => {
-    const ce = createCustomElement(AppComponent, { injector: appRef.injector });
-    customElements.define('av-quote-widget', ce);
-    connectRouter(appRef.injector.get(Router));
-  })
-  .catch((err) =>
-    console.error(err)
-  );
+(async () => {
+  const app = await createApplication(appConfig);
+  connectRouter(app.injector.get(Router));
+  const ce = createCustomElement(AppComponent, {
+    injector: app.injector,
+  });
+  customElements.define('av-quote-widget', ce);
+})();
+
+// bootstrapApplication(AppComponent, appConfig)
+//   .then((appRef: ApplicationRef) => {
+//     const ce = createCustomElement(AppComponent, { injector: appRef.injector });
+//     customElements.define('av-quote-widget', ce);
+//     connectRouter(appRef.injector.get(Router));
+//   })
+//   .catch((err) =>
+//     console.error(err)
+//   );
--- apps/ui/widgets/vk-tool/src/bootstrap.ts
+++ apps/ui/widgets/vk-tool/src/bootstrap.ts
@@ -1,4 +1,4 @@
-import { bootstrapApplication } from "@angular/platform-browser";
+import { bootstrapApplication, createApplication } from "@angular/platform-browser";
 import { ApplicationRef } from "@angular/core";
 import { Router } from "@angular/router";
 import { connectRouter } from "@angular-architects/module-federation-tools";
@@ -6,12 +6,21 @@ import { AppComponent } from "./app/app.component";
 import { appConfig } from "./app/app.config";
 import { createCustomElement } from "@angular/elements";
 
-bootstrapApplication(AppComponent, appConfig)
-  .then((appRef: ApplicationRef) => {
-    const ce = createCustomElement(AppComponent, { injector: appRef.injector });
-    customElements.define('vk-tool-widget', ce);
-    connectRouter(appRef.injector.get(Router));
-  })
-  .catch((err) =>
-    console.error(err)
-  );
+(async () => {
+  const app = await createApplication(appConfig);
+  connectRouter(app.injector.get(Router));
+  const ce = createCustomElement(AppComponent, {
+    injector: app.injector,
+  });
+  customElements.define('vk-tool-widget', ce);
+})();
+
+// bootstrapApplication(AppComponent, appConfig)
+//   .then((appRef: ApplicationRef) => {
+//     const ce = createCustomElement(AppComponent, { injector: appRef.injector });
+//     customElements.define('vk-tool-widget', ce);
+//     connectRouter(appRef.injector.get(Router));
+//   })
+//   .catch((err) =>
+//     console.error(err)
+//   );
